2a64a0f7cc4ac29a5a2453ae6a381805e58ba09c
Yahasaka <ignaciopicado.ar54@gmail.com>
2026-05-18 17:07:49 -0600

    feat: routes setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Catalog from './pages/Catalog'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalogo" element={<Catalog />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
